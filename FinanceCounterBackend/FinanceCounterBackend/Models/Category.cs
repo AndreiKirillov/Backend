a9b93ae9552ae3618e6805a5d9d12767c006ef9a
AndreiKirillov <AndreiKirillov@users.noreply.github.com>
2022-03-23 00:18:06 +0300

    Start

---
+++ FinanceCounterBackend/FinanceCounterBackend/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinanceCounterBackend.Models
+{
+    public class Category
+    {
+        private int Id { get; set; }
+
+        private string Name;
+
+        
+    }
+}
---
+++ FinanceCounterBackend/FinanceCounterBackend/Models/Payment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinanceCounterBackend.Models
+{
+    public class Payment
+    {
+        private int Id { get; set; }
+
+        private double Ammount;
+
+        private Category category;
+
+        [DataType(DataType.Date)]
+        private DateTime Date { get; set; }
+    }
+}
